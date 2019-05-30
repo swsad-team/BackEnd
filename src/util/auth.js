a9ad5e19dfde4eea4882560de86db8d527fa0aa2
Dennis273 <dennisc695@gmail.com>
2019-05-30 12:46:52 +0800

    增加身份验证中间件

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,3 +1,4 @@
+import User from '../model/user'
 import jwt from 'jsonwebtoken'
 import logger from './logger'
 import dotenv from 'dotenv'
@@ -6,6 +7,10 @@ dotenv.config()
 
 const expireTime = 60 * 60 * 24 // one day
 
+if (process.env.MODE === 'DEVELOPMENT') {
+  expireTime *= 365
+}
+
 const key = process.env.JWT_KEY // key should be defined in .env file
 
 /**
@@ -13,14 +18,10 @@ const key = process.env.JWT_KEY // key should be defined in .env file
  * @param {Object} payload claims to be stored in the token
  * @returns {string} jwt token
  */
-export const signJwtToken = (payload) => {
-  return jwt.sign(
-    payload,
-    key,
-    {
-      expiresIn: expireTime,
-    }
-  )
+export const signJwtToken = payload => {
+  return jwt.sign(payload, key, {
+    expiresIn: expireTime,
+  })
 }
 
 /**
@@ -28,11 +29,34 @@ export const signJwtToken = (payload) => {
  * @param {string} token jwt token
  * @returns {Object | undefined} decoded claims or `undefined` when err occurred
  */
-export const decodeJwtToken = (token) => {
+export const decodeJwtToken = token => {
   try {
     const payload = jwt.verify(token, key)
     return payload
   } catch (err) {
     return undefined
   }
+}
+
+/**
+ *
+ * @description This middle ware is used to add `user` property into request.
+ */
+export const authenticate = async (req, _, next) => {
+  if (
+    req.header('authorization') &&
+    req.header('authorization').startsWith('Bearer')
+  ) {
+    let token = req.header('authorization').split(' ')[1]
+    let payload = decodeJwtToken(token)
+    if (payload && payload.id !== undefined) {
+      const user = await User.findOne({
+        id: payload.id,
+      })
+      req.user = user
+    }
+  } else {
+    req.user = 'hello'
+  }
+  next()
 }
