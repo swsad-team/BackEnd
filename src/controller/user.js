21809114f05683d3effa01ad011ab2654d51716b
Dennis273 <dennisc695@gmail.com>
2019-05-30 12:47:33 +0800

    实现用户的登录、数据获取、修改等操作

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,28 +1,79 @@
 import decodeJwtToken from '../util/auth'
+import User from '../model/user'
+import { signJwtToken } from '../util/auth'
+import logger from '../util/logger'
 
-/**
- * 
- * @description This middle ware is used to add `user` property into request.
- */
-export const authenticate = (req, _, next) => {
-  if (req.header.authorization && req.header.authorization.startsWith('Bearer')) {
-    let token = req.header.authorization.split(' ')[1]
-    let payload = decodeJwtToken(token)
-    if (payload && payload['userId']) {
-      // find user by id here.
-    }
+// return JWT token
+export const login = async (req, res) => {
+  const identity = {
+    nicname: req.body.nickname || req.params.nickname,
+    email: req.body.email || req.params.email,
   }
-  next()
+  Object.keys(identity).forEach(
+    key => identity[key] === undefined && delete identity[key]
+  )
+  const password = req.body.password || req.params.password
+  const user = await User.findOne(identity)
+  if (user) {
+    if (user.comparePassword(password)) {
+      const token = signJwtToken(user.getPublicFields())
+      res.status(200).end(token)
+    } else {
+      res.status(401).end('PASSWORD_INCORRECT')
+    }
+  } else {
+    res.status(401).end('USER_NOT_FOUND')
+  }
 }
 
-export const updateUser = (req, res) => {
+export const logout = (req, res) => {}
 
+export const updateUser = async (req, res) => {
+  const targetUserId = req.params.id // string
+  const currentUserId = req.user.id // number
+  if (targetUserId !== currentUserId.toString()) res.status(403).end()
+  const updateData = req.body
+  try {
+    const user = await User.findOneAndUpdate({ id: targetUserId }, updateData, {
+      omitUndefined: true,
+      new: true,
+    })
+    res.status(200).json(user.getPublicFields())
+  } catch (err) {
+    res.status(400).end('ERROR')
+  }
 }
 
-export const getUsers = (req, res) => {
-
+export const createUser = async (req, res) => {
+  const newData = req.body
+  console.log(newData)
+  try {
+    const user = new User(newData)
+    await user.save()
+    res.status(200).json(user.getPublicFields())
+  } catch (err) {
+    logger.info(err)
+    res.status(400).end()
+  }
 }
 
-export const getUserById = (req, res) => {
-
+export const getUsers = async (req, res) => {
+  const condition = req.query
+  const userId = req.params.id
+  try {
+    if (userId === undefined) {
+      const users = await User.find(condition)
+      res.status(200).json(users.map(user => user.getPublicFields()))
+    } else {
+      const user = await User.findOne({ id: userId })
+      if (user) {
+        res.status(200).json(user.getPublicFields())
+      } else {
+        res.status(404).end()
+      }
+    }
+  } catch (err) {
+    logger.error(err)
+    res.status(400).end()
+  }
 }
--- src/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app'
-import logger from './util/logger'
-const server = app.listen(app.get('port'), () => {
-  logger.info(`Server running on port:${app.get('port')}`)
-})
